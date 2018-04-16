727641589a134fa549eb11e9cc48d53e956b5907
Usuario Local <Usuario Local>
2018-04-16 16:58:28 +0200

    Sprites y cosas

---
+++ Assets/Código/NuestroCodigo/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    
+    //GameObjects que vamos a usar.//
+    //Entidades
+    public GameObject heroe;
+    public GameObject aliado;
+    public GameObject enemigo;
+    //suelo
+    public GameObject suelo;
+    //Entorno
+    public GameObject casa;
+    public GameObject sol;
+    public GameObject luna;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //Primero, ponemos la casa y el agente.
+    //Después, puedes clickar en casillas para cambiar entre aliado zombie o nada.
+    //(Teniendo el cuenta los limites)
+    void creaTablero()
+    {
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
